e482d123d72754539926d115e7c492dcbd22b7af
iuzziel <iuzziel@hotmail.fr>
2017-10-13 12:30:25 +0200

    2nd push

--- app/src/main/java/afpa/convertisseur/MainActivity.java
+++ app/src/main/java/afpa/convertisseur/MainActivity.java
@@ -1,7 +1,9 @@
 package afpa.convertisseur;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onClick(View View) {
+        // traitement déclenché par le clic sur le bouton
+        // par exemple affichage d’une boite popup
+        Toast.makeText(getBaseContext(), "Message", Toast.LENGTH_LONG).show();
     }
 }
