9ca3341eee18eccdc481f37825dea76fb1d22ad8
iuzziel <iuzziel@hotmail.Fr>
2017-10-16 13:25:55 +0200

    Convertiseur terminée

---
+++ app/src/main/java/afpa/convertisseur/Convertisseur.java
@@ -0,0 +1,55 @@
+package afpa.convertisseur;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.text.DecimalFormat;
+
+import afpa.convertisseur.metier.Convert;
+
+public class Convertisseur extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_convertisseur);
+    }
+
+    public void onClick(View view) {
+        EditText editText = (EditText) findViewById(R.id.editMontant);
+        Spinner sSource = (Spinner) findViewById(R.id.spinDeviseDepart);
+        Spinner sCible = (Spinner) findViewById(R.id.spinDeviseArrivee);
+
+        DecimalFormat df = new DecimalFormat();
+        df.setMaximumFractionDigits(2);
+        df.setMinimumFractionDigits(0);
+
+        try {
+            String source = sSource.getSelectedItem().toString();
+            String cible = sCible.getSelectedItem().toString();
+            if (!(source.equals("")) && !(cible.equals("")) && !(source.equals(cible))) {
+                Double montant = Double.valueOf(editText.getText().toString());
+                double result = Convert.convertir(source, cible, montant);
+                Toast.makeText(getBaseContext(), String.valueOf(result), Toast.LENGTH_LONG).show();
+                TextView textView = (TextView) findViewById(R.id.tvResultat);
+                textView.setText(String.valueOf(df.format(result)));
+            } else {
+                Toast.makeText(getBaseContext(), "Selectionnez les devises", Toast.LENGTH_LONG).show();
+            }
+        } catch (Exception e) {
+            Toast.makeText(getBaseContext(), e.getMessage(), Toast.LENGTH_LONG).show();
+        }
+
+    }
+
+    public void onQuit(View view) {
+        Log.i("Fin", "Fermeture de l'application.");
+        System.exit(0);
+    }
+}
--- app/src/main/java/afpa/convertisseur/MainActivity.java
+++ app/src/main/java/afpa/convertisseur/MainActivity.java
@@ -1,17 +1,11 @@
 package afpa.convertisseur;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
-import android.widget.EditText;
-import android.widget.Spinner;
-import android.widget.TextView;
 import android.widget.Toast;
-
-import java.text.DecimalFormat;
-
-import afpa.convertisseur.metier.Convert;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -21,31 +15,10 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    public void onClick(View View) {
-        EditText editText = (EditText) findViewById(R.id.editMontant);
-        Spinner sSource = (Spinner) findViewById(R.id.spinDeviseDepart);
-        Spinner sCible = (Spinner) findViewById(R.id.spinDeviseArrivee);
-
-        DecimalFormat df = new DecimalFormat();
-        df.setMaximumFractionDigits(2);
-        df.setMinimumFractionDigits(0);
-
-        try {
-            String source = sSource.getSelectedItem().toString();
-            String cible = sCible.getSelectedItem().toString();
-            if (!(source.equals("")) && !(cible.equals("")) && !(source.equals(cible))) {
-                Double montant = Double.valueOf(editText.getText().toString());
-                double result = Convert.convertir(source, cible, montant);
-                Toast.makeText(getBaseContext(), String.valueOf(result), Toast.LENGTH_LONG).show();
-                TextView textView = (TextView) findViewById(R.id.tvResultat);
-                textView.setText(String.valueOf(df.format(result)));
-            } else {
-                Toast.makeText(getBaseContext(), "Selectionnez les devises", Toast.LENGTH_LONG).show();
-            }
-        } catch (Exception e) {
-            Toast.makeText(getBaseContext(), e.getMessage(), Toast.LENGTH_LONG).show();
-        }
-
+    public void onClick(View view) {
+        Toast.makeText(getBaseContext(), String.valueOf(findViewById(view.getId())), Toast.LENGTH_LONG).show();
+        Intent intent = new Intent(this, Convertisseur.class);
+        startActivity(intent);
     }
 
     public void onQuit(View view) {
