972460b6954669403a3a1e3eb30c99aa8f67d7bb
iuzziel <iuzziel@hotmail.Fr>
2017-10-17 11:42:54 +0200

    Test changement de police

--- app/src/main/java/afpa/convertisseur/MainActivity.java
+++ app/src/main/java/afpa/convertisseur/MainActivity.java
@@ -5,6 +5,9 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
 
@@ -25,5 +28,31 @@ public class MainActivity extends AppCompatActivity {
     public void onQuit(View view) {
         Log.i("Fin", "Fermeture de l'application.");
         System.exit(0);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        // Instanciation du menu XML spécifier en un objet Menu
+        inflater.inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // On teste l’Id de l’item cliqué et on déclenche une action
+        switch (item.getItemId()) {
+            case R.id.opt_convert:
+                Intent intent = new Intent(this, Convertisseur.class);
+                startActivity(intent);
+                return true;
+            case R.id.item2:
+                // action 2
+                return true;
+            case R.id.quitter:
+                finish();
+                return true;
+            default:
+                return false;
+        }
     }
 }
